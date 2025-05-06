2c73f561ba9bbe77b99922be9b0c7f33bba729f4
CGRowe96 <cgrowe1996@outlook.com>
2025-05-06 15:40:08 -0400

    autodownloader

--- D214/automater.py
+++ D214/automater.py
@@ -1,6 +1,33 @@
 import pyautogui as pag
+import time
 
 print(pag.size())
 
-pag.moveTo(45,200,duration=1)
-pag.click(45,200)
+def autodownloader(num,query):
+    pag.moveTo(110, 155, duration=.2)
+    pag.click(110, 155)
+    pag.typewrite(f'{query}')
+    pag.typewrite(['enter'])
+    pag.moveTo(110, 350, duration=.2)
+    
+    i = 0
+    while i < num:
+        time.sleep(1)
+        pag.click(110,350)
+        time.sleep(1)
+        pag.click(770,475)
+        time.sleep(70)
+        pag.click(300,200)
+        time.sleep(1)
+        pag.click(400,30,button='middle')
+        pag.click(75,30)
+        pag.click(80,155)
+        pag.moveTo(110,350)
+        pag.scroll(-215)
+        i += 1
+    
+    time.sleep(5)
+    pag.click(110, 155, clicks=3)
+    pag.typewrite(['backspace'])
+
+autodownloader(2,'Hospitals in West Virginia')
