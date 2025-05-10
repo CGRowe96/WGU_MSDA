5d3f825bae2270dceb13d420026e0c5044c5f42b
CGRowe96 <cgrowe1996@outlook.com>
2025-05-10 12:42:46 -0400

    finally

--- D214/automater pc.py
+++ D214/automater pc.py
@@ -57,3 +57,163 @@ print(pag.size())
         #pag.typewrite(['down'])
         #i += 1
 
+#def df_creation(num):
+    #i = 0
+    #pag.click(300,250)
+    #while i < num:
+        #name = i + 1
+        #pag.typewrite(f'df{name} = pd.read_csv("{name}.csv")')
+        #pag.typewrite(['enter'])
+        #i += 1
+
+def concat_auto(num):
+    i = 0
+    pag.click(453,482)
+    while i < num:
+        name = i + 1
+        pag.typewrite(f'df{name}, ')
+        i += 1
+        if i == 20:
+            pag.typewrite(['enter'])
+        if i == 40:
+            pag.typewrite(['enter'])
+        if i == 60:
+            pag.typewrite(['enter'])
+        if i == 80:
+            pag.typewrite(['enter'])
+        if i == 100:
+            pag.typewrite(['enter'])
+        if i == 120:
+            pag.typewrite(['enter'])
+        if i == 140:
+            pag.typewrite(['enter'])
+        if i == 160:
+            pag.typewrite(['enter'])
+        if i == 180:
+            pag.typewrite(['enter'])
+        if i == 200:
+            pag.typewrite(['enter'])
+        if i == 220:
+            pag.typewrite(['enter'])
+        if i == 240:
+            pag.typewrite(['enter'])
+        if i == 260:
+            pag.typewrite(['enter'])
+        if i == 280:
+            pag.typewrite(['enter'])
+        if i == 300:
+            pag.typewrite(['enter'])
+        if i == 320:
+            pag.typewrite(['enter'])
+        if i == 340:
+            pag.typewrite(['enter'])
+        if i == 360:
+            pag.typewrite(['enter'])
+        if i == 380:
+            pag.typewrite(['enter'])
+        if i == 400:
+            pag.typewrite(['enter'])
+        if i == 420:
+            pag.typewrite(['enter'])
+        if i == 440:
+            pag.typewrite(['enter'])
+        if i == 460:
+            pag.typewrite(['enter'])
+        if i == 480:
+            pag.typewrite(['enter'])
+        if i == 500:
+            pag.typewrite(['enter'])
+        if i == 520:
+            pag.typewrite(['enter'])
+        if i == 540:
+            pag.typewrite(['enter'])
+        if i == 560:
+            pag.typewrite(['enter'])
+        if i == 580:
+            pag.typewrite(['enter'])
+        if i == 600:
+            pag.typewrite(['enter'])
+        if i == 620:
+            pag.typewrite(['enter'])
+        if i == 640:
+            pag.typewrite(['enter'])
+        if i == 660:
+            pag.typewrite(['enter'])
+        if i == 680:
+            pag.typewrite(['enter'])
+        if i == 700:
+            pag.typewrite(['enter'])
+        if i == 720:
+            pag.typewrite(['enter'])
+        if i == 740:
+            pag.typewrite(['enter'])
+        if i == 760:
+            pag.typewrite(['enter'])
+        if i == 780:
+            pag.typewrite(['enter'])
+        if i == 800:
+            pag.typewrite(['enter'])
+        if i == 820:
+            pag.typewrite(['enter'])
+        if i == 840:
+            pag.typewrite(['enter'])
+        if i == 860:
+            pag.typewrite(['enter'])
+        if i == 880:
+            pag.typewrite(['enter'])
+        if i == 900:
+            pag.typewrite(['enter'])
+        if i == 920:
+            pag.typewrite(['enter'])
+        if i == 940:
+            pag.typewrite(['enter'])
+        if i == 960:
+            pag.typewrite(['enter'])
+        if i == 980:
+            pag.typewrite(['enter'])
+        if i == 1000:
+            pag.typewrite(['enter'])
+        if i == 1020:
+            pag.typewrite(['enter'])
+        if i == 1040:
+            pag.typewrite(['enter'])
+        if i == 1060:
+            pag.typewrite(['enter'])
+        if i == 1080:
+            pag.typewrite(['enter'])
+        if i == 1100:
+            pag.typewrite(['enter'])
+        if i == 1120:
+            pag.typewrite(['enter'])
+        if i == 1140:
+            pag.typewrite(['enter'])
+        if i == 1160:
+            pag.typewrite(['enter'])
+        if i == 1180:
+            pag.typewrite(['enter'])
+        if i == 1200:
+            pag.typewrite(['enter'])
+        if i == 1220:
+            pag.typewrite(['enter'])
+        if i == 1240:
+            pag.typewrite(['enter'])
+        if i == 1260:
+            pag.typewrite(['enter'])
+        if i == 1280:
+            pag.typewrite(['enter'])
+        if i == 1300:
+            pag.typewrite(['enter'])
+        if i == 1320:
+            pag.typewrite(['enter'])
+        if i == 1340:
+            pag.typewrite(['enter'])
+        if i == 1360:
+            pag.typewrite(['enter'])
+        if i == 1380:
+            pag.typewrite(['enter'])
+        if i == 1400:
+            pag.typewrite(['enter'])
+        if i == 1420:
+            pag.typewrite(['enter'])
+        if i == 1440:
+            pag.typewrite(['enter'])
